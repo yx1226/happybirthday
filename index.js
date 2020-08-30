9887a815f8f0bf83f5053bc2b4523a1231d7eea9
Yuxian Aurora <61455400+yx1226@users.noreply.github.com>
2020-08-30 22:35:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 
 const config = {
   birthdate: 'August 31, 2020',
-  name: '宇衔'
+  name: 'Malaysia'
 };
 
 function hideEverything() {
@@ -48,7 +48,7 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY', 'BIRTHDAY!', config.name],
+      strings: ['HAPPY', 'INDEPENDENCE', 'DAY!',config.name],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
